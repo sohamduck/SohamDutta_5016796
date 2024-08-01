f57ed7baa10168c25188ca81a506329cdbe2fb7b
Soham Dutta <duttasoham2222@gmail.com>
2024-08-01 23:04:38 +0530

    Exercise 1 done

---
+++ Week 2/librarymanagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,13 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    public void printBookRepository(){
+        System.out.println("BookRepository Instance : "+bookRepository);
+    }
+}
