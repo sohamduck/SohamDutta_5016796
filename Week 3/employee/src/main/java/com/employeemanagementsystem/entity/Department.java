482a08a82bf5d4abeb091d0414f20a6ad9521dfe
Soham Dutta <duttasoham2222@gmail.com>
2024-08-08 02:46:38 +0530

    Exercise 7 done FINAL

--- Week 3/employee/src/main/java/com/employeemanagementsystem/entity/Department.java
+++ Week 3/employee/src/main/java/com/employeemanagementsystem/entity/Department.java
@@ -1,6 +1,10 @@
 package com.employeemanagementsystem.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @NamedQueries({
     @NamedQuery(
         name="Department.findByName",
@@ -31,4 +36,13 @@ public class Department {
     private String name;
     @OneToMany(mappedBy = "department")
     private List<Employee> employees;
+
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
--- Week 3/employee/src/main/java/com/employeemanagementsystem/entity/Employee.java
+++ Week 3/employee/src/main/java/com/employeemanagementsystem/entity/Employee.java
@@ -1,6 +1,7 @@
 package com.employeemanagementsystem.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -10,11 +11,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.annotation.*;
+
+
 @Entity
 @Table(name="employees")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Employee {
     @Id
     @GeneratedValue
@@ -25,4 +32,13 @@ public class Employee {
     @ManyToOne
     @JoinColumn(name="department_id")
     private Department department;
+
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
